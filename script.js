a44aea08b13fbd0c3549f9f0f2ab33100e2db4af
= <=>
2023-11-06 19:26:16 +0400

    commit  olundu

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Beden kutle indeksi kutlenin hundurlutyun kvadratina olan nisbetidir!
 
 //1.Nihad ve Nailin ceki ve boyunu deyisende saxla!
-let NihadBoy = 1.69;
-let NihadCeki = 78;
-let NailBoy = 1.95;
-let NailCeki = 92;
+let NihadBoy1 = 1.69;
+let NihadCeki1 = 78;
+let NailBoy2 = 1.95;
+let NailCeki2 = 92;
 
 //2.onlarin Bki hesabla!
-NihadBkI = nihadCeki / NihadBoy ** 2;
-NailBkI = nailCeki / nailBoy ** 2;
+NihadBkI = NihadCeki /( NihadBoy ** 2);
+NailBkI = NailCeki / (NailBoy ** 2);
 //3. Nihadin BKI Nailden boyukdurmu?
 if (NihadBkI > NailBkI) {
     console.log(true);
@@ -25,14 +25,14 @@ let Data1 = Data1NihadBKI > Data1NailBKI;
 
 console.log(Data1);
 
-let Data2NihadKg = 95;
-let Data2NihadM = 1.88;
+let Data2NihadCeki1 = 95;
+let Data2NihadBoy1 = 1.88;
 
-let Data2NailKq = 85;
-let Data2NailM = 1.76;
+let Data2NailCeki2 = 85;
+let Data2NailBoy2 = 1.76;
 
-let Data2NihadBKI = Data2NihadKq / (Data2NihadM ** 2);
-let Data2NailBKI = Data2NailKq / (Data2NailM ** 2);
+let Data2NihadBKI = Data2NihadCeki / (Data2NihadBoy ** 2);
+let Data2NailBKI = Data2NailCeki / (Data2NailBoy ** 2);
 //Data 2.
 //Nihad 95 kq - 188sm
 //Nail 85kq -176sm
